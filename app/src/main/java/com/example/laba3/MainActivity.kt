47c1f9e393b02a9291784c63d6051a6d68e05d2d
Ufgt_NOI <strelok1579@gmail.com>
2025-01-04 19:59:09 +0700

    Добавляем алгоритм замены цифр на пробелы

--- app/src/main/java/com/example/laba3/MainActivity.kt
+++ app/src/main/java/com/example/laba3/MainActivity.kt
@@ -1,20 +1,41 @@
 package com.example.laba3
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val inputField = findViewById<EditText>(R.id.inputField)
+        val checkButton = findViewById<Button>(R.id.checkButton)
+        val resultText = findViewById<TextView>(R.id.resultText)
+
+        checkButton.setOnClickListener {
+            val input = inputField.text.toString()
+
+            var result = ""
+            var sum = 0
+            for (char in input) {
+                if (char.isDigit()) {
+                    result += " "
+                }
+                else if (char.isLowerCase()){
+                    sum += 1
+                    result += char
+                }
+                else result += char
+            }
+            resultText.text = "$result  $sum"
         }
     }
 }
